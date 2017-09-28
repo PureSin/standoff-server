b69268fb7ca8e1ef615552e5b65154caf9950f0d
Richard Worsfold <worsfold@google.com>
2017-09-28 10:07:08 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+
+io.on('connection', () => {
+
+})
+
+server.listen(3000)
